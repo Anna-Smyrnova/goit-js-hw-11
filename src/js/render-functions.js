2d85e056f7aca44477dce3aad20c258d0252052a
Anna Smyrnova <anyto4ka777s@gmail.com>
2026-02-14 23:11:38 +0200

    add js files

---
+++ src/js/render-functions.js
@@ -0,0 +1,17 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+function createGallery () {};
+
+function clearGallery () {};
+
+function showLoader () {};
+
+function hideLoader () {};
+
+
+export {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader
+};
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import getImagesByQuery from './js/pixabay-api.js';
+import {
+    createGallery,
+    clearGallery,
+    showLoader,
+    hideLoader
+} from './js/render-functions.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
